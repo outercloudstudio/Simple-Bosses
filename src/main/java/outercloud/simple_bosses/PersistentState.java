0ad4195b4de15bd969f207fc30a2aac34a54ade5
Outer Cloud Studio <outercloudstudio@gmail.com>
2023-11-11 03:51:20 -0600

    working moves

--- src/main/java/outercloud/simple_bosses/PersistentState.java
+++ src/main/java/outercloud/simple_bosses/PersistentState.java
@@ -14,7 +14,14 @@ import java.util.UUID;
 
 public class PersistentState extends net.minecraft.world.PersistentState {
     private HashMap<String, UUID> bosses = new HashMap<>();
-    private HashMap<String, List<String>> moves = new HashMap<>();
+    private HashMap<String, String> bossStates = new HashMap<>();
+    private HashMap<String, String> bossCurrentMoves = new HashMap<>();
+    private HashMap<String, Integer> bossStateProgresses = new HashMap<>();
+    private HashMap<String, ArrayList<String>> moves = new HashMap<>();
+    private HashMap<String, ArrayList<Integer>> moveCooldowns = new HashMap<>();
+    private HashMap<String, ArrayList<Integer>> moveDurations = new HashMap<>();
+    private HashMap<String, ArrayList<Integer>> moveWindups = new HashMap<>();
+    private HashMap<String, ArrayList<Integer>> moveRecovers = new HashMap<>();
 
     @Override
     public NbtCompound writeNbt(NbtCompound nbt) {
@@ -41,12 +48,27 @@ public class PersistentState extends net.minecraft.world.PersistentState {
         PersistentState instance = get(server);
         instance.bosses.put(name, entity.getUuid());
         instance.moves.put(name, new ArrayList<>());
+        instance.moveCooldowns.put(name, new ArrayList<>());
+        instance.moveDurations.put(name, new ArrayList<>());
+        instance.moveRecovers.put(name, new ArrayList<>());
+        instance.moveWindups.put(name, new ArrayList<>());
+        instance.bossStates.put(name, "Cooldown");
+        instance.bossStateProgresses.put(name, 0);
+        instance.bossCurrentMoves.put(name, "None");
     }
 
     public static void removeBoss(String name, MinecraftServer server) {
         PersistentState instance = get(server);
+
         instance.bosses.remove(name);
+        instance.bossStates.remove(name);
+        instance.bossStateProgresses.remove(name);
+        instance.bossCurrentMoves.remove(name);
         instance.moves.remove(name);
+        instance.moveCooldowns.remove(name);
+        instance.moveDurations.remove(name);
+        instance.moveWindups.remove(name);
+        instance.moveRecovers.remove(name);
     }
 
     public static Entity getBoss(String name, MinecraftServer server) {
@@ -61,21 +83,92 @@ public class PersistentState extends net.minecraft.world.PersistentState {
         return  null;
     }
 
-    public static void addMove(String boss, String name, MinecraftServer server) {
+    public static String getState(String name, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.bossStates.get(name);
+    }
+
+    public static void setState(String name, String state, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        instance.bossStates.put(name, state);
+    }
+
+    public static int getProgress(String name, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.bossStateProgresses.get(name);
+    }
+
+    public static void setProgress(String name, int progress, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        instance.bossStateProgresses.put(name, progress);
+    }
+
+    public static void addMove(String boss, String name, int cooldown, int duration, int windup, int recover, MinecraftServer server) {
         PersistentState instance = get(server);
         instance.moves.get(boss).add(name);
+        instance.moveCooldowns.get(boss).add(cooldown);
+        instance.moveDurations.get(boss).add(duration);
+        instance.moveWindups.get(boss).add(windup);
+        instance.moveRecovers.get(boss).add(recover);
     }
 
     public static void removeMove(String boss, String name, MinecraftServer server) {
         PersistentState instance = get(server);
-        instance.moves.get(boss).remove(name);
+
+        int index = instance.moves.get(boss).indexOf(name);
+
+        instance.moves.get(boss).remove(index);
+        instance.moveCooldowns.get(boss).remove(index);
+        instance.moveDurations.get(boss).remove(index);
+        instance.moveWindups.get(boss).remove(index);
+        instance.moveRecovers.get(boss).remove(index);
     }
 
-    public static List<String> getBossNames(MinecraftServer server) {
-        return get(server).bosses.keySet().stream().toList();
+    public static String getMove(String name, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.bossCurrentMoves.get(name);
     }
 
-    public static List<String> getBossMoves(String name, MinecraftServer server) {
-        return get(server).moves.get(name);
+    public static void setMove(String name, String move, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        instance.bossCurrentMoves.put(name, move);
+    }
+
+    public static int getDuration(String name, String move, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.moveDurations.get(name).get(instance.moves.get(name).indexOf(move));
+    }
+
+    public static int getWindup(String name, String move, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.moveWindups.get(name).get(instance.moves.get(name).indexOf(move));
+    }
+
+    public static int getRecover(String name, String move, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.moveRecovers.get(name).get(instance.moves.get(name).indexOf(move));
+    }
+
+    public static int getCooldown(String name, String move, MinecraftServer server) {
+        PersistentState instance = get(server);
+
+        return instance.moveCooldowns.get(name).get(instance.moves.get(name).indexOf(move));
+    }
+
+    public static ArrayList<String> getBossNames(MinecraftServer server) {
+        return new ArrayList<>(get(server).bosses.keySet());
+    }
+
+    public static ArrayList<String> getBossMoves(String name, MinecraftServer server) {
+        return new ArrayList<>(get(server).moves.get(name));
     }
 }
--- src/main/java/outercloud/simple_bosses/SimpleBosses.java
+++ src/main/java/outercloud/simple_bosses/SimpleBosses.java
@@ -3,19 +3,29 @@ package outercloud.simple_bosses;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import outercloud.simple_bosses.lib.Commands;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class SimpleBosses implements ModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("simple_bosses");
 
+	private static final Random RANDOM = new Random();
+
 	@Override
 	public void onInitialize() {
+		ServerTickEvents.END_SERVER_TICK.register(this::tick);
+
 		Commands.create("boss", command -> command
 				.requires(source -> source.hasPermissionLevel(4))
 				.then(CommandManager.literal("create")
@@ -32,53 +42,48 @@ public class SimpleBosses implements ModInitializer {
 								)
 						)
 				)
-				.then(CommandManager.literal("edit")
+				.then(CommandManager.literal("moves")
 						.then(CommandManager.argument("boss_name", StringArgumentType.word())
-								.then(CommandManager.literal("moves")
-										.then(CommandManager.literal("add")
-												.then(CommandManager.argument("move_name", StringArgumentType.word())
-														.executes(context -> {
-															String boss = StringArgumentType.getString(context, "boss_name");
-															String move = StringArgumentType.getString(context, "move_name");
-
-															LOGGER.info(boss);
-															LOGGER.info(move);
-
-															PersistentState.addMove(boss, move, context.getSource().getServer());
-
-															return 1;
-														})
-												)
-										)
-										.then(CommandManager.literal("remove")
-												.then(CommandManager.argument("move_name", StringArgumentType.word())
-														.executes(context -> {
-															String boss = StringArgumentType.getString(context, "boss_name");
-															String move = StringArgumentType.getString(context, "move_name");
-
-															PersistentState.removeMove(boss, move, context.getSource().getServer());
-
-															return 1;
-														})
-												)
-										)
-										.then(CommandManager.literal("edit"))
-										.then(CommandManager.literal("list")
+								.then(CommandManager.literal("add")
+										.then(CommandManager.argument("move_name", StringArgumentType.word())
 												.executes(context -> {
-													String name = StringArgumentType.getString(context, "boss_name");
+													String boss = StringArgumentType.getString(context, "boss_name");
+													String move = StringArgumentType.getString(context, "move_name");
 
-													context.getSource().sendMessage(Text.of("Moves of " + name + ":"));
-
-													int index = 1;
-													for(String moveName: PersistentState.getBossMoves(name, context.getSource().getServer())) {
-														context.getSource().sendMessage(Text.of(index + ". " + moveName));
-
-														index++;
-													}
+													PersistentState.addMove(boss, move, 20, 0, 0, 0, context.getSource().getServer());
 
 													return 1;
 												})
 										)
+								)
+								.then(CommandManager.literal("remove")
+										.then(CommandManager.argument("move_name", StringArgumentType.word())
+												.executes(context -> {
+													String boss = StringArgumentType.getString(context, "boss_name");
+													String move = StringArgumentType.getString(context, "move_name");
+
+													PersistentState.removeMove(boss, move, context.getSource().getServer());
+
+													return 1;
+												})
+										)
+								)
+								.then(CommandManager.literal("edit"))
+								.then(CommandManager.literal("list")
+										.executes(context -> {
+											String name = StringArgumentType.getString(context, "boss_name");
+
+											context.getSource().sendMessage(Text.of("Moves of " + name + ":"));
+
+											int index = 1;
+											for(String moveName: PersistentState.getBossMoves(name, context.getSource().getServer())) {
+												context.getSource().sendMessage(Text.of(index + ". " + moveName));
+
+												index++;
+											}
+
+											return 1;
+										})
 								)
 						)
 				)
@@ -114,5 +119,58 @@ public class SimpleBosses implements ModInitializer {
 						)
 				)
 		);
+	}
+
+	private void tick(MinecraftServer server) {
+		for(String name : PersistentState.getBossNames(server)) {
+			Entity boss = PersistentState.getBoss(name, server);
+
+			int progress = PersistentState.getProgress(name, server);
+
+			PersistentState.setProgress(name, progress - 1, server);
+
+			if(progress > 0) continue;
+
+			String state = PersistentState.getState(name, server);
+
+			if(state == "Cooldown") {
+				ArrayList<String> moves = PersistentState.getBossMoves(name, server);
+
+				if(moves.size() == 0) continue;
+
+				String newMove = moves.get(RANDOM.nextInt(moves.size()));
+
+				PersistentState.setState(name, "Windup", server);
+				PersistentState.setMove(name, newMove, server);
+				PersistentState.setProgress(name, PersistentState.getWindup(name, newMove, server), server);
+
+				LOGGER.info("Switched from Cooldown to Windup on " + name);
+			} else if(state == "Windup") {
+				String move = PersistentState.getMove(name, server);
+
+				PersistentState.setState(name, "Move", server);
+				PersistentState.setProgress(name, PersistentState.getDuration(name, move, server), server);
+
+				LOGGER.info("Switched from Windup to Move on " + name);
+			} else if(state == "Move") {
+				String move = PersistentState.getMove(name, server);
+
+				PersistentState.setState(name, "Recover", server);
+				PersistentState.setProgress(name, PersistentState.getRecover(name, move, server), server);
+
+				boss.addCommandTag("boss_move_" + move);
+
+				LOGGER.info("Switched from Move to Recover on " + name);
+			} else if(state == "Recover") {
+				String move = PersistentState.getMove(name, server);
+
+				PersistentState.setState(name, "Cooldown", server);
+				PersistentState.setProgress(name, PersistentState.getCooldown(name, move, server), server);
+
+				boss.removeScoreboardTag("boss_move_" + move);
+
+				LOGGER.info("Switched from Recover to Cooldown on " + name);
+			}
+		}
 	}
 }
